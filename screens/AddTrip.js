505eeb56d7cb7f23b147b509743591bef9865291
Takahiro Murakami <tayuusfka@gmail.com>
2023-01-11 22:14:43 +0900

    feat: made add trip page

--- screens/AddTrip.js
+++ screens/AddTrip.js
@@ -1,4 +1,113 @@
-import React from 'react';
+import { Formik } from 'formik';
+import React, { useState } from 'react';
+import { StyleSheet, Button, TextInput, View, TouchableOpacity } from 'react-native';
+import { Octicons, Ionicons, Entypo } from '@expo/vector-icons';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import {
+    StyledContainer,
+    StyledTextInput,
+    LeftIcon,
+    StyledInputLabel,
+    Colors, 
+} from '../styles/styles';
+
+const {brand, darkLight} = Colors;
+
 export default function AddTrip(params) {
+  const [show, setShow] = useState(false);
+  const [date, setDate] = useState(new Date(2023, 0, 1));
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(false);
+    setDate(currentDate);
+    setStartDate(currentDate);
+    setEndDate(currentDate);
+  }
+
+  const showDatePicker = () => {
+    setShow(true);
+  }
+
+  return(
+    <View style={StyledContainer}>
+
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode='date'
+          is24Hour={true}
+          onChange={onChange}
+        />
+      )}
+
+      <Formik
+        initialValues={{ startDate: '', endDate: '', userid: '', name: '' }}
+        onSubmit={(values) =>{
+          values = {...values, startDate: startDate, endDate: endDate}
+          console.log(values);
+        }}
+      >
+        {(props) => (
+          <View>
+            <MyTextInput 
+              label="Trip Name"
+              icon="airplane-sharp"
+              placeholder="Go to Japan"
+              onChangeText={props.handleChange('name')}
+              value={props.values.name}
+            />
+
+            <MyTextInput 
+              label="Start Date"
+              icon="calendar"
+              placeholder="YYYY-MM-DD"
+              onChangeText={props.handleChange('startDate')}
+              value={startDate ? startDate.toDateString() : ''}
+              isDate={true}
+              editable={false}
+              showDatePicker={showDatePicker}
+            />
+
+            <MyTextInput 
+              label="End Date"
+              icon="calendar"
+              placeholder="YYYY-MM-DD"
+              onChangeText={props.handleChange('endDate')}
+              value={endDate ? endDate.toDateString() : ''}
+              isDate={true}
+              editable={false}
+              showDatePicker={showDatePicker}
+             
+            />
+
+            <Button title='Add new trip' color= 'maroon' onPress={props.handleSubmit} />
+          </View>
+        )}
+      </Formik>
+    </View>
+
+  );
   
+};
+
+const MyTextInput = ( { label, icon, isDate, showDatePicker, ...props }) => {
+    return (
+        <View>
+            <LeftIcon>
+                <Ionicons name={icon} size={30} color={brand} />
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+            {!isDate && <StyledTextInput {...props} />}
+            {isDate && (
+              <TouchableOpacity onPress={showDatePicker}>
+                <StyledTextInput {...props} />
+              </TouchableOpacity>
+            )}
+            
+        </View>
+    );
 };
--- screens/Trips.js
+++ screens/Trips.js
@@ -1,12 +1,15 @@
 import React, { useState, useContext } from 'react'
-import { StyleSheet, Text, FlatList, View, TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, Text, FlatList, View, TouchableOpacity, Button, Modal } from 'react-native';
 import { AuthContext } from '../context/AuthContext';
 import { InfoContext } from '../context/InfoContext';
-import { StyledButton, ButtonText } from '../styles/styles';
+import { StyledButton, ButtonText, SubTitle } from '../styles/styles';
+import { MaterialIcons } from '@expo/vector-icons';
+import AddTrip from './AddTrip';
 
 export default function Trips({ navigation }) {
   const { logout } = useContext(AuthContext); 
   const { trips } = useContext(InfoContext); 
+  const [modalOpen, setModalOpen] =useState(false);
 
   const pressHandler = (item) => {
     navigation.navigate('TripTabNav', {
@@ -19,6 +22,29 @@ export default function Trips({ navigation }) {
   return (
     <>
     <View style={styles.container}>
+
+      <Modal visible={modalOpen} animationType='slide'>
+        <View style={StyleSheet.modalContent}>
+          <MaterialIcons
+            name="close"
+            size={24}
+            style={{ ...styles.modalToggle, ...styles.modalClose }}
+            onPress={() => setModalOpen(false)}
+          />
+          <AddTrip />
+        </View>
+      </Modal>
+  
+      <MaterialIcons
+        name="add"
+        size={24}
+        style={styles.modalToggle}
+        onPress={() => setModalOpen(true)}
+      />
+      <View>
+        <Text style={styles.text}>Add new trip!</Text>
+      </View>
+
       <FlatList
         keyExtractor={( item ) => item.id}
         data = {trips}
@@ -56,5 +82,25 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     borderRadius: 6,
     backgroundColor:'#A020F0'
-  }
+  },
+  modalToggle: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: 'center'
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0,
+  },
+  modalContent: {
+    flex: 1,
+  },
+  text: {
+    marginBottom: 10,
+    padding: 10,
+    alignSelf: 'center'
+  },
 })
