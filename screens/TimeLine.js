32130c9cf6ed0e667c33c5d96efb980bca84ece3
YJK-7 <2019yjk@gmail.com>
2023-01-12 11:50:10 +0900

    fix: touch up style

--- screens/TimeLine.js
+++ screens/TimeLine.js
@@ -67,7 +67,7 @@ export default function TimeLine ({ route, navigation }) {
   return (
     <>
       <View style={styles.container}>
-        
+        <View style={styles.innerContainer}>
         <FlatList
           keyExtractor={(item) => item.id}
           data={tripEvents}
@@ -82,17 +82,18 @@ export default function TimeLine ({ route, navigation }) {
               style={{...styles.modalToggle, ...styles.modalClose}}
               onPress={() => setModalOpen(false)}
             />
-            <AddTimeline/>
+            <AddTimeline setModalOpen={setModalOpen}/>
           </View>
         </Modal>
         
-        <TouchableOpacity onPress={() => setModalOpen(true)}>
-          <MaterialCommunityIcons
-            name='plus-circle'
-            size={50}
-            style={styles.modalToggle}
-          />
-        </TouchableOpacity>
+          <TouchableOpacity onPress={() => setModalOpen(true)} style={styles.iconContainer}>
+              <MaterialCommunityIcons
+                name='plus'
+                size={50}
+                style={styles.modalToggle}
+              />
+          </TouchableOpacity>
+        </View>
       </View>
     </>
   )
@@ -100,6 +101,10 @@ export default function TimeLine ({ route, navigation }) {
 
 const styles = StyleSheet.create({
   container:{
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  innerContainer:{
     flex: 1,
     backgroundColor: '#fff',
     marginHorizontal: 10,
@@ -111,13 +116,35 @@ const styles = StyleSheet.create({
     padding: 20,
     marginTop: 10,
     borderRadius: 6,
-    backgroundColor:'#A020F0'
+    backgroundColor:'#9CCAEC'
+  },
+
+  iconContainer:{
+    alignItems:"center",
+    alignSelf:"flex-end",
+    backgroundColor:'#F187A4',
+    borderRadius: 40,
+    justiftyContent:"center",
+    margin:5,
+    
+    height:70,
+    width:70,
+    
+    position:"absolute",
+    right:0,
+    bottom:10,
+
+    shadowColor: "#000",
+    shadowOpacity: 0.9,
+    shadowRadius: 5,
+    elevation: 7,
   },
 
   dayEvent:{
     fontSize: 24,
     padding: 20
   },
+
   modalContent:{
     flex:1,
     margin:10,
@@ -125,7 +152,7 @@ const styles = StyleSheet.create({
   modalToggle:{
     margin:10,
     alignSelf:"flex-end",
-    color:'#A020F0'
+    color:'#fff',
   },
   modalClose:{
     margin:0,
