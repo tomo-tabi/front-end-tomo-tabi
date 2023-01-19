b44a9328f3f7280ec684e2120e56565d827df5c2
Eric <eytam619@gmail.com>
2023-01-19 05:35:23 -0500

    feat: implement autoCapitalize signup email input

--- screens/Signup.js
+++ screens/Signup.js
@@ -80,6 +80,7 @@ const Signup = ({ navigation }) => {
                               placeholder="xxx@gmail.com"
                               placeholderTextColor={darkLight}
                               onChangeText={handleChange('email')}
+                              autoCapitalize="none"
                               onBlur={handleBlur('email')}
                               value={values.email}
                               keyboardType="email-address"
