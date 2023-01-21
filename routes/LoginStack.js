04c8c4fc468f9b19168adf4b6b285a0cc1c2bb7a
YJK-7 <2019yjk@gmail.com>
2023-01-21 19:54:18 +0900

    design: remove login header

--- routes/LoginStack.js
+++ routes/LoginStack.js
@@ -1,13 +1,17 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from '../screens/Login';
-import Signup from '../screens/Signup';
+import { Login } from '../screens/Login';
+import { Signup } from '../screens/Signup'
 
 const Stack = createNativeStackNavigator();
 
 export default function LoginStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name='Login' component={Login}/>
+      <Stack.Screen name='Login' component={Login} 
+        options={({route}) => ({
+          headerShown:false
+        })} 
+      />
       <Stack.Screen name='Signup' component={Signup}/>
     </Stack.Navigator>
   )
