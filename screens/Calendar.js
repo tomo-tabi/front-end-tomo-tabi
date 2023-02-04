00992a4777f6f71114ac3abb5576da2d747fa5d2
YJK-7 <2019yjk@gmail.com>
2023-02-04 10:57:32 +0900

    stop code after set func

--- screens/Calendar.js
+++ screens/Calendar.js
@@ -172,10 +172,11 @@ export default function CalendarView(params) {
             // console.log("eventArrFormat",eventArrFormat);
 
             setDayViewDate(new Date(day));
-            if (currentEventArr && currentEventArr.length !== 0) {
-                return setDayViewData(currentEventArr);
+
+            if (currentEventArr && currentEventArr.length !== 0) { //
+                return setDayViewData(currentEventArr); //
             } else {
-                return setDayViewData([]);
+                return setDayViewData([]); //
             }
         }
     }
